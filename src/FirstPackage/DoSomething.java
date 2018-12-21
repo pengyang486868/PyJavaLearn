f60d34032017e0b256bb3ab8348a4f67c1774713
yang peng <854525261@qq.com>
2018-12-21 19:35:50 +0800

    add

--- src/FirstPackage/DoSomething.java
+++ src/FirstPackage/DoSomething.java
@@ -1,10 +1,13 @@
 package FirstPackage;
 
+import secondPackage.secondClass;
+
 public class DoSomething {
 
 	public static void main(String[] args) {
 		System.out.println("py");
-
+		secondClass sc = new secondClass();
+		System.out.println(sc.a);
 	}
 
 }
---
+++ src/secondPackage/secondClass.java
@@ -0,0 +1,9 @@
+package secondPackage;
+
+public class secondClass {
+	public int a;
+
+	public secondClass() {
+		a = 2;
+	}
+}
